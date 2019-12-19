a3005457f97f27d03e63ab2564caa2fbefc571c2
Patrick Colina <trick.co@yahoo.com>
2019-12-19 06:51:06 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,20 +1,7 @@
 def turn_count(board)
-  counter = 0
-  board.each do |value|
-    if (value == "X" || value == "O")
-      counter += 1
-    end
-  end
-  counter
+  board.count{|token| token == "X" || token == "O"}
 end
 
 def current_player(board)
-  player = "X"
-  counter = turn_count(board)
-    if counter.even?
-        player = "X"
-    else
-        player = "O"
-    end
-  player
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
